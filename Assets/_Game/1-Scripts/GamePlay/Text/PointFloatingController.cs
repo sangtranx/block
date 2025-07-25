d3a159539d44a85ee47a9034e33dcd63dd6d0097
David <David@Sang-Tran.local>
2025-07-25 20:48:43 +0700

    update

--- Assets/_Game/1-Scripts/GamePlay/Text/PointFloatingController.cs
+++ Assets/_Game/1-Scripts/GamePlay/Text/PointFloatingController.cs
@@ -9,9 +9,9 @@ public class PointFloatingController : MonoBehaviour
     public void Spawn(int value, Vector2 startPos, float time = 0.5f)
     {
         // instanPlace = GameObject.FindGameObjectWithTag("TextInstancePlace");
-        var f = Instantiate(pointFloatingPrefab, startPos, Quaternion.identity, instanPlace.transform);
-        var ui = f.GetComponent<PointFloatingUI>();
-        ui.Set(value, time);
+        //var f = Instantiate(pointFloatingPrefab, startPos, Quaternion.identity, instanPlace.transform);
+        //var ui = f.GetComponent<PointFloatingUI>();
+        //ui.Set(value, time);
     }
     private void Update()
     {
