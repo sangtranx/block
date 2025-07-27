4d8869a96ad527b9d67e729b242c886b7bc63186
baotranunitydev <baotranunitydev@gmail.com>
2025-07-27 22:47:34 +0700

    disable spawn combo

--- Assets/_Game/1-Scripts/GamePlay/Text/ComboTextController.cs
+++ Assets/_Game/1-Scripts/GamePlay/Text/ComboTextController.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public class ComboTextController : MonoBehaviour
 {
-    [SerializeField] GameObject comboTextPrefab;
-    [SerializeField] GameObject instanPlace;
-    public void Spawn(int value)
+  [SerializeField] GameObject comboTextPrefab;
+  [SerializeField] GameObject instanPlace;
+  public void Spawn(int value)
+  {
+    Debug.Log("Value Spawn: " + value);
+    //instanPlace = GameObject.FindGameObjectWithTag("TextInstancePlace");
+    // if (value <= 0) return;
+    // AudioController.Instance.Play(AudioName.Sound_Clear);
+    // var f = Instantiate(comboTextPrefab, new Vector2(0, 1), Quaternion.identity, instanPlace.transform);
+    // var ui = f.GetComponent<ComboTextUI>();
+    // ui.Set(value);
+  }
+  private void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Space))
     {
-        //instanPlace = GameObject.FindGameObjectWithTag("TextInstancePlace");
-        if(value <= 0) return;
-        AudioController.Instance.Play(AudioName.Sound_Clear);
-        var f = Instantiate(comboTextPrefab, new Vector2(0, 1), Quaternion.identity, instanPlace.transform);
-        var ui = f.GetComponent<ComboTextUI>();
-        ui.Set(value);
+      Spawn(Random.Range(1, 4));
     }
-    private void Update() 
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Spawn(Random.Range(1, 4));
-        }
-    }
+  }
 }
