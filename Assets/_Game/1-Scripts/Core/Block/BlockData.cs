1920432d3ca5f7f8c114e069178941e5587a3006
baotranunitydev <baotranunitydev@gmail.com>
2025-07-27 15:26:44 +0700

    update

--- Assets/_Game/1-Scripts/Core/Block/BlockData.cs
+++ Assets/_Game/1-Scripts/Core/Block/BlockData.cs
@@ -6,30 +6,30 @@ using UnityEngine;
 
 namespace Game.Core.Block
 {
-    public class BlockData : MonoBehaviour
+  public class BlockData : MonoBehaviour
+  {
+    [SerializeField] private SkinBlockSO _skinBlockSo;
+    private ShopDB _shopDB => DBController.Instance.SHOP_DB;
+
+    public SpirteSkinBlock RandomData()
     {
-        [SerializeField] private SkinBlockSO _skinBlockSo;
-        private ShopDB _shopDB => DBController.Instance.SHOP_DB;
+      var currentSkinBlockType = _shopDB.GetShopModelByType((ShopCategory.BLOCK)).currentRegion;
+      var skin = _skinBlockSo.GetSkinByType(currentSkinBlockType);
+      if (skin == null)
+      {
+        return null;
+      }
 
-        public SpirteSkinBlock RandomData()
-        {
-            var currentSkinBlockType = _shopDB.GetShopModelByType((ShopCategory.BLOCK)).currentRegion;
-            var skin = _skinBlockSo.GetSkinByType(currentSkinBlockType);
-            if (skin == null)
-            {
-                return null;
-            }
-
-            var level = DBController.Instance.USER_DATA.GetResourceByType(TypeResource.Level).amount;
-            var id = UnityEngine.Random.Range(0, Mathf.Clamp(6 + (int)(level % 3), 6, 10));
-            return skin.lstSprIcon[id];
-        }
+      var level = DBController.Instance.USER_DATA.GetResourceByType(TypeResource.Level).amount;
+      var id = UnityEngine.Random.Range(0, Mathf.Clamp(6 + (int)(level % 3), 6, 7));
+      return skin.lstSprIcon[id];
     }
+  }
 
-    [Serializable]
-    public class BlockSkin
-    {
-        public int id;
-        public Sprite sprBlock;
-    }
+  [Serializable]
+  public class BlockSkin
+  {
+    public int id;
+    public Sprite sprBlock;
+  }
 }
